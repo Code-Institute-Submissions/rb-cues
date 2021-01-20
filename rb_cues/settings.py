749a90e89fa2e8a0d4532ea919684100a06bf14c
VolkovBos <volkovbos@gmail.com>
2021-01-20 20:28:18 +0000

    Added crispy fields and Add Product page

--- rb_cues/settings.py
+++ rb_cues/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     # Created apps
     'home',
     'products',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'rb_cues.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -76,6 +81,10 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+            ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
             ],
         },
     },

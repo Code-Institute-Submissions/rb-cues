e3c4b8befb75931ac8ee69f633a578ae85809c5a
VolkovBos <volkovbos@gmail.com>
2021-01-20 20:42:29 +0000

    Added tests

--- products/test_urls.py
+++ products/test_urls.py
@@ -1,14 +1,28 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from products.views import all_products, product_detail
+from products.views import (
+    all_products, product_detail, add_product, edit_product, delete_product
+)
 
 
 class TestUrls(SimpleTestCase):
 
-    def test_houses_url_resolves(self):
+    def test_products_url_resolves(self):
         url = reverse('products')
         self.assertEquals(resolve(url).func, all_products)
 
-    def test_house_info_url_resolves(self):
+    def test_product_detail_url_resolves(self):
         url = reverse('product_detail', args=[1])
         self.assertEquals(resolve(url).func, product_detail)
+
+    def test_add_product_url_resolves(self):
+        url = reverse('add_product')
+        self.assertEquals(resolve(url).func, add_product)
+
+    def test_edit_product_url_resolves(self):
+        url = reverse('edit_product', args=[1])
+        self.assertEquals(resolve(url).func, edit_product)
+
+    def test_delete_product_url_resolves(self):
+        url = reverse('delete_product', args=[1])
+        self.assertEquals(resolve(url).func, delete_product)

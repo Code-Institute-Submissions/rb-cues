c3ece4ffb389a226f7d9c45c6a4fc1728ad83bea
VolkovBos <volkovbos@gmail.com>
2021-01-20 15:33:56 +0000

    Product details added

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 # The url for my products app
 urlpatterns = [
-    path('', views.all_products, name='products')
+    path('', views.all_products, name='products'),
+    path('<product_id>', views.product_detail, name='product_detail'),
 ]

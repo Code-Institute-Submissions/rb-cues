ad60a1efd1e13695de9ae163703137be346a72b2
VolkovBos <volkovbos@gmail.com>
2021-01-25 12:55:54 +0000

    Ran migration to postgress Heroku

--- rb_cues/settings.py
+++ rb_cues/settings.py
@@ -126,6 +126,8 @@ WSGI_APPLICATION = 'rb_cues.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
